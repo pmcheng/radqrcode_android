2e549121081817c924ae16bb767db08a49db1439
Phillip Cheng <phillip.cheng@usc.edu>
2012-08-22 23:07:29 -0700

    Button edit of Date Numeric keypad for MRN

--- CaseQRCode/src/pmcheng/caseqrcode/EditActivity.java
+++ CaseQRCode/src/pmcheng/caseqrcode/EditActivity.java
@@ -1,12 +1,15 @@
 package pmcheng.caseqrcode;
 
-import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -18,18 +21,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-
 
 public class EditActivity extends Activity {
 	private static final String TAG = "EditActivity";
 	long case_id;
-	EditText e_mrn, e_loc, e_study, e_date, e_desc;
+	EditText e_mrn, e_loc, e_study, e_desc;
+	Button b_date;
 	CheckBox cb_fu;
 	CaseQRCodeApp caseApp;
 	boolean newCase = false;
 	boolean spinnerInitialized = false;
+
+	private int mYear;
+	private int mMonth;
+	private int mDay;
+
+	static final int DATE_DIALOG_ID = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +50,7 @@ public class EditActivity extends Activity {
 		e_mrn = (EditText) findViewById(R.id.editMRN);
 		e_loc = (EditText) findViewById(R.id.editLoc);
 		e_study = (EditText) findViewById(R.id.editStudy);
-		e_date = (EditText) findViewById(R.id.editDate);
+		b_date = (Button) findViewById(R.id.btnDate);
 		e_desc = (EditText) findViewById(R.id.editDesc);
 		cb_fu = (CheckBox) findViewById(R.id.checkBoxFollow);
 
@@ -49,9 +59,11 @@ public class EditActivity extends Activity {
 		caseApp = (CaseQRCodeApp) super.getApplication();
 		Cursor c = null;
 		if (case_id == -1) {
-			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-			String date = dateFormat.format(new Date());
-			e_date.setText(date);
+			Calendar cal = Calendar.getInstance();
+			mYear = cal.get(Calendar.YEAR);
+			mMonth = cal.get(Calendar.MONTH);
+			mDay = cal.get(Calendar.DAY_OF_MONTH);
+			b_date.setText(mYear + "-" + (mMonth + 1) + "-" + mDay);
 
 			newCase = true;
 
@@ -69,7 +81,22 @@ public class EditActivity extends Activity {
 				e_study.setText(study);
 
 				String date = c.getString(c.getColumnIndex(CaseData.C_DATE));
-				e_date.setText(date);
+				b_date.setText(date);
+
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				Calendar cal = Calendar.getInstance();
+				try {
+					Date d = sdf.parse(date);
+					cal.setTime(d);
+				} catch (ParseException e) {
+					Log.v(TAG, e.getMessage());
+				}
+				mYear = cal.get(Calendar.YEAR);
+				mMonth = cal.get(Calendar.MONTH);
+				mDay = cal.get(Calendar.DAY_OF_MONTH);
+
+				Log.v(TAG, "Parsed date = " + mYear + "-" + (mMonth + 1) + "-"
+						+ mDay);
 
 				String desc = c.getString(c.getColumnIndex(CaseData.C_DESC));
 				e_desc.setText(desc);
@@ -79,6 +106,13 @@ public class EditActivity extends Activity {
 			}
 		}
 
+		b_date.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+
 		Button btnUpdate = (Button) findViewById(R.id.btnUpdate);
 		btnUpdate.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -86,7 +120,7 @@ public class EditActivity extends Activity {
 				case_array[0] = e_loc.getText().toString();
 				case_array[1] = e_mrn.getText().toString();
 				case_array[2] = e_study.getText().toString();
-				case_array[3] = e_date.getText().toString();
+				case_array[3] = b_date.getText().toString();
 				case_array[4] = e_desc.getText().toString();
 				case_array[5] = cb_fu.isChecked() ? "1" : "0";
 
@@ -106,10 +140,10 @@ public class EditActivity extends Activity {
 		locList.add(0, "---");
 
 		Spinner locSpinner = (Spinner) findViewById(R.id.spinnerLoc);
-		if (locList.size()>1) {
+		if (locList.size() > 1) {
 			try {
 				ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-						android.R.layout.simple_spinner_item,locList);
+						android.R.layout.simple_spinner_item, locList);
 				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 				locSpinner.setAdapter(adapter);
 
@@ -119,8 +153,9 @@ public class EditActivity extends Activity {
 									View v, int pos, long id) {
 								if (!spinnerInitialized) {
 									spinnerInitialized = true;
-								} else if (pos>0){
-									e_loc.setText(parent.getItemAtPosition(pos).toString());
+								} else if (pos > 0) {
+									e_loc.setText(parent.getItemAtPosition(pos)
+											.toString());
 								}
 							}
 
@@ -180,4 +215,31 @@ public class EditActivity extends Activity {
 				WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
 	}
+
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DATE_DIALOG_ID:
+			return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,
+					mDay);
+		}
+		return null;
+	}
+
+	private void updateDisplay() {
+		String date = mYear + "-" + String.format("%02d", mMonth + 1) + "-"
+				+ String.format("%02d", mDay);
+		b_date.setText(date);
+	}
+
+	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			mYear = year;
+			mMonth = monthOfYear;
+			mDay = dayOfMonth;
+			updateDisplay();
+		}
+	};
+
 }
