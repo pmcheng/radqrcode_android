ee320ff5b2afe458788b721790a6207e5d3df4a8
Phillip Cheng <phillip.cheng@usc.edu>
2016-06-25 09:57:52 -0700

    Set minimum SDK to 9 Fix for upside-down camera on Nexus 5X and other devices.

--- captureActivity/src/main/java/com/google/zxing/client/android/camera/CameraConfigurationManager.java
+++ captureActivity/src/main/java/com/google/zxing/client/android/camera/CameraConfigurationManager.java
@@ -24,6 +24,8 @@ import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
 
+import android.view.Surface;
+
 import java.util.regex.Pattern;
 
 final class CameraConfigurationManager {
@@ -70,8 +72,35 @@ final class CameraConfigurationManager {
     parameters.setPreviewSize(cameraResolution.x, cameraResolution.y);
     setFlash(parameters);
     setZoom(parameters);
+    setCameraDisplayOrientation(context,Camera.CameraInfo.CAMERA_FACING_BACK,camera);
     camera.setParameters(parameters);
   }
+
+    public static void setCameraDisplayOrientation(Context context,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
 
   Point getCameraResolution() {
     return cameraResolution;
